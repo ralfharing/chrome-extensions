7e579f1e3ee3ed5cbcb07c2d6efed7abfc6cbd65
Ralf Haring <github@preypacer.com>
2012-06-28 17:13:40 -0400

    version 1.5

--- show_all_pages/extension/show_all_pages.js
+++ show_all_pages/extension/show_all_pages.js
@@ -12,42 +12,43 @@ $(document).ready(function(){
         // On profile these will be the first three, but if you
         // are a page it will be the page plus the first two.
         //
-        // .JUa = the <a> hover elements (among others)
+        // .aJa = the <a> hover elements (among others)
         // a[href*="/b/"] = only those that contain link to brand pages
         // contents() = <img> and text nodes for each
         // .filter(nodeType = 3) = only the text nodes
-        var existingHoverEntries = $('.JUa').filter('a[href*="/b/"]').contents().filter(function(){return(this.nodeType == 3);});
+        var existingHoverEntries = $('.aJa').filter('a[href*="/b/"]').contents().filter(function(){return(this.nodeType == 3);});
 
         // convert text nodes to strings
         $.each(existingHoverEntries, function(index, val){existingHoverEntries[index] = val.nodeValue;})
 
-        // .JBb.a-f-e.lrb = a page entry on the management page
-        $('.JBb.a-f-e.lrb', data).each(function(){
-            // .qda = div with the name
-            var name = $(this).find('.qda').text();
+        // .OYb.a-f-e.YOb = a page entry on the management page
+        $('.OYb.a-f-e.YOb', data).each(function(){
+            // .Ira = div with the name
+            var name = $(this).find('.Ira').text();
             // if the name is already on the hovercard, do nothing
             if($.inArray(name, existingHoverEntries) != -1) return;
 
-            // .l-mj.Qf = img tag with big picture
+            // .l-tk.Rf = img tag with big picture
             // src = only the src property
             // replace(.s102) = get small thumbnails
             // replace(https) = chop the protocol
-            var thumbnail = $(this).find('.l-mj.Qf').prop('src').replace('s102-c-k', 's32-c-k').replace('https:', '');
-            // .HBb = a tag with the page link
+            var thumbnail = $(this).find('.l-tk.Rf').prop('src').replace('s102-c-k', 's32-c-k').replace('https:', '');
+            // .MYb = a tag with the page link
             // href = only the href property
             // replace() = chop the protocal and domain
-            var link = $(this).find('.HBb').prop('href').replace('https://plus.google.com', '');
+            var link = $(this).find('.MYb').prop('href').replace('https://plus.google.com', '');
 
-            // .JUa = the hovercard entries
+            // .aJa = the hovercard entries
             // a[href*="/b/"] = only those that contain link to brand pages
             // .last() = no way to determine which is which from class data
-            var lastHoverEntry = $('.JUa').filter('a[href*="/b/"]').last();
+            var lastHoverEntry = $('.aJa').filter('a[href*="/b/"]').last();
 
             // make copy to hold new info and populate it
+            // .Zb.AWa = img tag with the hover entry thumbnail
             var newHoverEntry = lastHoverEntry.clone();
             newHoverEntry.contents().last().replaceWith(name)
             newHoverEntry.prop('href', link);
-            newHoverEntry.find('.Zb.Z7a').prop('src', thumbnail);
+            newHoverEntry.find('.Zb.AWa').prop('src', thumbnail);
 
             // insert it before the current last hover entry
             newHoverEntry.insertAfter(lastHoverEntry);
