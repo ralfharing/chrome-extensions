4beb3973df30aca7cf22821c8add2b2b9f300ed5
Ralf Haring <github@preypacer.com>
2013-03-09 14:46:41 -0500

    de_instant_mix v1.1.2 - decrease initial delay

--- de_instant_mix/extension/de_instant_mix.js
+++ de_instant_mix/extension/de_instant_mix.js
@@ -5,10 +5,15 @@
 // container for all albums, instant mixes, and playlists
 var album_pane = $('#start-page-new-section')[0];
 
-// create an observer to delete the instant mixes
-var observer = new WebKitMutationObserver(function(){
+// does the work
+var remove_mixes = function(){
     $('div[data-type="im"]').parent().remove();
-});
+}
 
+// create an observer to delete the instant mixes
+var observer = new WebKitMutationObserver(remove_mixes);
 // watch for new children to be added
 observer.observe(album_pane, {childList: true});
+
+// call once while page content still loading offscreen
+remove_mixes();
