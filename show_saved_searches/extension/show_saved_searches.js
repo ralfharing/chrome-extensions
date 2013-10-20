0b2f8c85bae4dfad936d87b17bccee00fe249f08
Ralf Haring <ralf.haring@gmail.com>
2013-10-20 19:16:43 -0400

    show_saved_searches v1.7 - store saved searches - fixes #13

--- show_saved_searches/extension/show_saved_searches.js
+++ show_saved_searches/extension/show_saved_searches.js
@@ -1,25 +1,14 @@
 /**
- * @author Ralf Haring 2013-10-15
+ * @author Ralf Haring 2013-10-20
  */
 
-var doWork = function(){
-    // searches are only present at first in a script tag
-    // parse it to get human readable text and create hrefs
+var storage = chrome.storage.sync;
 
-    // strip out newlines and empty array bits that break JSON.parse
-    var searchText = $("script:contains('key: \'23\'')").text().replace(/\n/g, '').replace(/\[,,?\[/g, '[[');
-    // strip out function surrounding everything
-    var startPos = searchText.indexOf('["osi.gc"');
-    searchText = searchText.substr(startPos, searchText.length - startPos - 3);
-    var searchArray = JSON.parse(searchText)[1];
-    var searches = [];
+// TODO: add method of saving new searches
+// TODO: add method of removing saved searches
 
-    for(var j=0; j < searchArray.length; j++){
-        // push elements like ["#human readable", "%23human%20readable"]
-        searches.push([searchArray[j][1], encodeURIComponent(searchArray[j][1])]);
-    }
-
-    // loop and construct the searches card
+function makeSearchCard(searches){
+    // loop and construct the searches card in the G+ main stream
 
     // make card have two columns, so split it for easy adding during loop
     var firstPart = '<div tabindex="-1" class="nja"><div class="Ee fP Ue lk Ls" role="article"><div class="a5 Gi"><h3 class="EY Ni zj"><span>Saved Searches</span></h3></div><div class="B3 Kg"><div class="xj"><div class="p3"><ul class="Bx wg">';
@@ -38,9 +27,69 @@ var doWork = function(){
     }
 
     var searchCard = firstPart + middlePart + lastPart;
-
     // insert after the share card
     $(searchCard).insertAfter('div[data-iid="sii2:111"]');
+}
+
+var doWork = function(){
+    // array of human readable searches and urlized versions
+    var searches = [];
+
+    // searches are only present at first in a script tag.
+    // parse it to get human readable text and create hrefs.
+    // strip out newlines and empty array bits that break JSON.parse.
+    var searchText = $("script:contains('key: \'23\'')").text().replace(/\n/g, '').replace(/\[,,?\[/g, '[[');
+
+    // on 15 November 2013 when Google turns off saved searches, the above script
+    // tag presumably won't exist anymore. in that case, retrieve the saved
+    // searches from chrome.storage instead.
+    if(searchText == ""){
+        storage.get(null, function(obj){
+            var stored_searches = [];
+            // convert the storage object to an array to sort
+            for(x in obj){
+                stored_searches.push([x, obj[x]]);
+            }
+            // pass in custom sort function for newest-first
+            stored_searches.sort(function(a, b){
+                return b[1] - a[1];
+            });
+            // drop the dates and create the searches array using the search
+            // text and the encoded version for the actual link
+            searches = stored_searches.map(function(y){return [y[0], encodeURIComponent(y[0])]});
+            makeSearchCard(searches);
+        });
+    }else{
+        // strip out function surrounding everything
+        var startPos = searchText.indexOf('["osi.gc"');
+        searchText = searchText.substr(startPos, searchText.length - startPos - 3);
+        var searchArray = JSON.parse(searchText)[1];
+        // just the human readable parts for saving to chrome.storage
+        var simple_searches = [];
+
+        for(var j=0; j < searchArray.length; j++){
+            // push elements like ["#human readable", "%23human%20readable"]
+            searches.push([searchArray[j][1], encodeURIComponent(searchArray[j][1])]);
+            simple_searches.push(searchArray[j][1]);
+        }
+
+        // reverse the array to get the timestamps right, earliest first
+        simple_searches.reverse();
+        storage.get(simple_searches, function(obj){
+            // check if an entry already exists and if not save a new timestamp
+            for(var k=0; k < simple_searches.length; k++){
+//                console.log("checking " + k + " " + simple_searches[k]);
+                var search_name = simple_searches[k];
+                if(! obj.hasOwnProperty(search_name)){
+//                    console.log("saving " + search_name);
+                    var o = {};
+                    o[search_name] = new Date().valueOf();
+                    storage.set(o);
+                }
+            }
+        });
+        makeSearchCard(searches);
+    }
 };
 
 // wait until page is fully loaded
